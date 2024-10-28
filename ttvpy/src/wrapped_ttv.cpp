f621b2638a5b395dc3788c0cab697d2f590b6563
Cem Bassoy <cem.bassoy@gmail.com>
2024-10-28 14:57:14 +0100

    fix(ttvpy): adjust ttv tags

--- ttvpy/src/wrapped_ttv.cpp
+++ ttvpy/src/wrapped_ttv.cpp
@@ -23,57 +23,56 @@ ttv(std::size_t const contraction_mode,
     py::array_t<T> const& b)
 {
 
-  using namespace tlib::ttv;
+    using namespace tlib::ttv;
 
-  auto const q = contraction_mode;
+    auto const q = contraction_mode;
+
+    auto const sizeofT = sizeof(T);
+
+    auto const& ainfo = a.request(); // request a buffer descriptor from Python of type py::buffer_info
+    auto const p  = std::size_t(ainfo.ndim); //py::ssize_t  
+
+    if(p==0)        throw std::invalid_argument("Error calling ttvpy::ttv: input tensor order should be greater than zero.");
+    if(q==0 || q>p) throw std::invalid_argument("Error calling ttvpy::ttv: contraction mode should be greater than zero or less than or equal to p.");  
+
+    auto const*const aptr = static_cast<T const*const>(ainfo.ptr);    // extract data an shape of input array  
+    auto na        = std::vector<std::size_t>(ainfo.shape  .begin(), ainfo.shape  .end());
+    auto wa        = std::vector<std::size_t>(ainfo.strides.begin(), ainfo.strides.end());    
+    //auto nna       = ainfo.size;  
+    std::for_each(wa.begin(), wa.end(), [sizeofT](auto& w){w/=sizeofT;});
+
+
+    auto       pia = std::vector<size_t>(p);
+    for(auto i = p; i > 0; --i) pia.at(p-i) = i;
+
+    auto const& binfo = b.request(); // request a buffer descriptor from Python of type py::buffer_info
+    auto const*const bptr = static_cast<T const*const>(binfo.ptr);    // extract data an shape of input array
+    auto const nb  = std::vector<std::size_t>(binfo.shape.begin(), binfo.shape.end());
+    //auto const nnb = binfo.size;
+    //auto const pb  = binfo.ndim;
   
-  auto const sizeofT = sizeof(T);
-  
-  auto const& ainfo = a.request(); // request a buffer descriptor from Python of type py::buffer_info
-  auto const p  = std::size_t(ainfo.ndim); //py::ssize_t  
+    auto const nc  = tlib::ttv::detail::generate_output_shape (na ,q);
+    auto const pic = tlib::ttv::detail::generate_output_layout(pia,q);	
+    auto       wc  = tlib::ttv::detail::generate_strides(nc,pic);	
 
-	if(p==0)        throw std::invalid_argument("Error calling ttvpy::ttv: input tensor order should be greater than zero.");
-	if(q==0 || q>p) throw std::invalid_argument("Error calling ttvpy::ttv: contraction mode should be greater than zero or less than or equal to p.");  
-  
-  auto const*const aptr = static_cast<T const*const>(ainfo.ptr);    // extract data an shape of input array  
-  auto na        = std::vector<std::size_t>(ainfo.shape  .begin(), ainfo.shape  .end());
-  auto wa        = std::vector<std::size_t>(ainfo.strides.begin(), ainfo.strides.end());    
-  //auto nna       = ainfo.size;  
-  std::for_each(wa.begin(), wa.end(), [sizeofT](auto& w){w/=sizeofT;});
+    auto       nc_ = std::vector<py::ssize_t>(nc.begin(),nc.end());
+    auto       wc_ = std::vector<py::ssize_t>(wc.begin(),wc.end());
+    std::for_each(wc_.begin(), wc_.end(), [sizeofT](auto& w){w*=sizeofT;});
 
 
-  auto       pia = std::vector<size_t>(p);
-  for(auto i = p; i > 0; --i) pia.at(p-i) = i;
+    auto c            = py::array_t<T>(nc_,wc_);
+    auto const& cinfo = c.request(); // request a buffer descriptor from Python of type py::buffer_info
+    auto* cptr        = static_cast<T*>(cinfo.ptr);    // extract data an shape of input array  
+    auto nnc          = std::size_t(cinfo.size);
 
-  auto const& binfo = b.request(); // request a buffer descriptor from Python of type py::buffer_info
-  auto const*const bptr = static_cast<T const*const>(binfo.ptr);    // extract data an shape of input array
-  auto const nb  = std::vector<std::size_t>(binfo.shape.begin(), binfo.shape.end());
-  //auto const nnb = binfo.size;
-  //auto const pb  = binfo.ndim;
-  
-	auto const nc  = tlib::ttv::detail::generate_output_shape (na ,q);
-	auto const pic = tlib::ttv::detail::generate_output_layout(pia,q);	
-	auto       wc  = tlib::ttv::detail::generate_strides(nc,pic);	
-
-	auto       nc_ = std::vector<py::ssize_t>(nc.begin(),nc.end());
-	auto       wc_ = std::vector<py::ssize_t>(wc.begin(),wc.end());
-  std::for_each(wc_.begin(), wc_.end(), [sizeofT](auto& w){w*=sizeofT;});
-
-  
- 	auto c            = py::array_t<T>(nc_,wc_);
-  auto const& cinfo = c.request(); // request a buffer descriptor from Python of type py::buffer_info
-  auto* cptr        = static_cast<T*>(cinfo.ptr);    // extract data an shape of input array  
-  auto nnc          = std::size_t(cinfo.size);
-
-  std::fill(cptr, cptr+nnc,T{});
-
+    std::fill(cptr, cptr+nnc,T{});
   
 #ifndef _OPENMP
-  tensor_times_vector<T>(tlib::execution::seq,   tlib::slicing::large, tlib::loop_fusion::none, q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
-#elif defined(USE_OPENBLAS) || defined(USE_MKLBLAS)
-  tensor_times_vector<T>(tlib::execution::blas,  tlib::slicing::large, tlib::loop_fusion::all , q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
+    tensor_times_vector<T>(tlib::execution_policy::seq,       tlib::slicing_policy::subtensor, tlib::fusion_policy::none, q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
+#elif defined(USE_OPENBLAS) || defined(USE_MKL)
+    tensor_times_vector<T>(tlib::execution_policy::par_loop,  tlib::slicing_policy::subtensor, tlib::fusion_policy::all , q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
 #else 
-  tensor_times_vector<T>(tlib::execution::par,   tlib::slicing::large, tlib::loop_fusion::none, q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
+    tensor_times_vector<T>(tlib::execution_policy::par,       tlib::slicing_policy::subtensor, tlib::fusion_policy::none, q, p, aptr, na.data(), wa.data(), pia.data(),  bptr, nb.data(),  cptr, nc.data(), wc.data(), pic.data());
 #endif
 
   return c;  
